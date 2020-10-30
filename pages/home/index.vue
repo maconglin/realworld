02d009865425205c9e6d36408b181f7a1f82e446
@云里 <masir_a@163.com>
2020-10-30 09:28:35 +0800

    部署测试

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -4,7 +4,7 @@
 
   <div class="banner">
     <div class="container">
-      <h1 class="logo-font">Nuxt.cool</h1>
+      <h1 class="logo-font">NuxtCool</h1>
       <p>A place to share your knowledge.</p>
     </div>
   </div>
